b754cee9b0f30445a2422fdae8d23140ae919559
Sangaransj <sangaranjk52@gmail.com>
2021-02-06 20:28:46 +0530

    Practice java

---
+++ src/org/test/BrowserLaunch.java
@@ -0,0 +1,15 @@
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserLaunch {
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\lenovo\\eclipse-workspace\\SeleniumDay1\\lib\\driver" );
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.cricbuzz.com");
+	}
+
+}
